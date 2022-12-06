920459eda7fe22a15567c377b2b2446e32cf59e4
vadimkain <48841785+vadimkain@users.noreply.github.com>
2022-12-06 18:19:21 +0200

    5. Statement. DDL операции

--- src/JdbcRunner.java
+++ src/JdbcRunner.java
@@ -5,18 +5,69 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
- * <h1>Подключение к базам данных</h1>
- * Класс Connection наследуется от AutoCloseable, т.е. необходимо закрывать все соединения, которые используем
- * чтобы предотвращать все утечки памяти.
- * <br><br>
- * Для хорошего тона выносим соединение в отдельный Util класс, все сведения о нём там же.
+ * <h1>Statement. DDL операции</h1>
+ * Для того, чтобы отправить запрос в БД у нас есть три варианта:
+ * <ol>
+ *     <li>
+ *         <b>Statement</b> - сам интерфейс. Он так-же наследуется от AutoCloseable, а значит его также надо
+ *         закрывать после выполнения запроса, как и соединение.
+ *     </li>
+ *     <li>
+ *         <b>CallableStatement</b> - не используем, т.к. он служит нам для выполнения хранимых процедур.
+ *     </li>
+ *     <li>
+ *         <b>PreparedStatement</b> - в реальной практике чаще всего используют именно этот класс, потому что он
+ *         более универсальный и наследуется от Statement.
+ *     </li>
+ * </ol>
+ * Для того чтобы выполнить универсальный запрос есть обычный метод <i>execute(String):boolean</i>, но он не очень
+ * удобен в плане получения какой-то информации, поэтому у нас есть другие похожие методы такие как например
+ * <i>executeUpdate(String):int</i>, который так же принимает строку и возвращает int, а не boolean. И так же
+ * <i>executeQuery(String):ResultSet</i>, который используется для выполнения SELECT'ов.
+ * <ul>
+ *     <li>
+ *         <b>execute(String):boolean</b> - универсальный, чаще для DDL операций
+ *     </li>
+ *     <li>
+ *         <b>executeQuery(String):ResultSet</b> - для SELECT'ов
+ *     </li>
+ *     <li>
+ *         <b>executeUpdate(String):int</b> - для DML операций (INSERT UPDATE DELETE)
+ *     </li>
+ *     <li>
+ *         <b>executeLargeUpdate(String):long</b> - для больших DML операций
+ *     </li>
+ * </ul>
  */
 public class JdbcRunner {
     public static void main(String[] args) {
         Class<Driver> driverClass = Driver.class;
+        /*
+        String sql = """
+                CREATE DATABASE game;
+                """;
+        String sql = """
+                DROP DATABASE game;
+                """;
+        String sql = """
+                CREATE SCHEMA game;
+                """;
 
-        try (var connection = ConnectionManager.open();) {
+         */
+        String sql = """
+                CREATE TABLE IF NOT EXISTS info (
+                    id SERIAL PRIMARY KEY,
+                    data TEXT NOT NULL
+                );
+                """;
+        try (
+                var connection = ConnectionManager.open();
+                var statement = connection.createStatement();
+        ) {
+            System.out.println(connection.getSchema());
             System.out.println(connection.getTransactionIsolation());
+            var executeResult = statement.execute(sql); // Выполнение DDL запроса
+            System.out.println(executeResult);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
