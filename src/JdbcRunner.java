13cc31a354b35d578ff5f3979c990b8788b3b428
vadimkain <48841785+vadimkain@users.noreply.github.com>
2022-12-06 18:30:47 +0200

    6. Statement. DML операции

--- src/JdbcRunner.java
+++ src/JdbcRunner.java
@@ -1,73 +1,38 @@
 import org.postgresql.Driver;
 import util.ConnectionManager;
 
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
- * <h1>Statement. DDL операции</h1>
- * Для того, чтобы отправить запрос в БД у нас есть три варианта:
- * <ol>
- *     <li>
- *         <b>Statement</b> - сам интерфейс. Он так-же наследуется от AutoCloseable, а значит его также надо
- *         закрывать после выполнения запроса, как и соединение.
- *     </li>
- *     <li>
- *         <b>CallableStatement</b> - не используем, т.к. он служит нам для выполнения хранимых процедур.
- *     </li>
- *     <li>
- *         <b>PreparedStatement</b> - в реальной практике чаще всего используют именно этот класс, потому что он
- *         более универсальный и наследуется от Statement.
- *     </li>
- * </ol>
- * Для того чтобы выполнить универсальный запрос есть обычный метод <i>execute(String):boolean</i>, но он не очень
- * удобен в плане получения какой-то информации, поэтому у нас есть другие похожие методы такие как например
- * <i>executeUpdate(String):int</i>, который так же принимает строку и возвращает int, а не boolean. И так же
- * <i>executeQuery(String):ResultSet</i>, который используется для выполнения SELECT'ов.
- * <ul>
- *     <li>
- *         <b>execute(String):boolean</b> - универсальный, чаще для DDL операций
- *     </li>
- *     <li>
- *         <b>executeQuery(String):ResultSet</b> - для SELECT'ов
- *     </li>
- *     <li>
- *         <b>executeUpdate(String):int</b> - для DML операций (INSERT UPDATE DELETE)
- *     </li>
- *     <li>
- *         <b>executeLargeUpdate(String):long</b> - для больших DML операций
- *     </li>
- * </ul>
+ * <h1>Statement. DML операции</h1>
+ * Дело в том, что через execute мы получаем только true или false. Но помним, что стандарты SQL говорят о том, что
+ * при выполнении INSERT/UPDATE/DELETE нам возвращается количество обновлённых/вставленных/удалённых строк. Через
+ * операцию execute нам пришлось бы доплнительно вызывать метод (<i>execute.getUpdateCount()</i>).
+ * <br><br>
+ * На практике для DML операций чаще используется <b>executeUpdate()</b>.
+ * <br>
  */
 public class JdbcRunner {
     public static void main(String[] args) {
         Class<Driver> driverClass = Driver.class;
         /*
         String sql = """
-                CREATE DATABASE game;
+                INSERT INTO info (data) VALUES ('Test1'), ('Test2'), ('Test3'), ('Test4');
                 """;
-        String sql = """
-                DROP DATABASE game;
-                """;
-        String sql = """
-                CREATE SCHEMA game;
-                """;
-
          */
         String sql = """
-                CREATE TABLE IF NOT EXISTS info (
-                    id SERIAL PRIMARY KEY,
-                    data TEXT NOT NULL
-                );
-                """;
+                UPDATE info SET data = 'TestTest' WHERE id = 5 RETURNING *;
+                """; // RETURNING * возвращаем сразу же значения обновленных строк, но с executeUpdate не работает
         try (
                 var connection = ConnectionManager.open();
                 var statement = connection.createStatement();
         ) {
             System.out.println(connection.getSchema());
             System.out.println(connection.getTransactionIsolation());
-            var executeResult = statement.execute(sql); // Выполнение DDL запроса
+            var executeResult = statement.executeUpdate(sql); // Выполнение DML запроса
             System.out.println(executeResult);
+            // Возвращаем кол-во строк, над которыми выполнили операции
+            System.out.println(statement.getUpdateCount());
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
