7e0e4a31cbd185b00f0c8b0220085fd55f91da16
vadimkain <48841785+vadimkain@users.noreply.github.com>
2022-12-08 17:34:58 +0200

    10. PreparedStatement

--- src/JdbcRunner.java
+++ src/JdbcRunner.java
@@ -1,62 +1,103 @@
 import util.ConnectionManager;
 
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * <h1>SQL Injection</h1>
- * При чём тут SQL Injection? Дело в том, что {@code Statement} редко пользуются, либо не пользуются вообще и
- * дают предпочтение именно {@code PreparedStatement} из-за SQL инъекций. Т.е. по сути это что-то вроде хакерских
- * атак, когда вместе с параметром (в данном случае это параметр {@code String flightId}). Поэтому эта строчка может
- * быть передана на сервер (в метод) видоизменнёной. Т.е. можем вместо "2" сказать: {@code 2 OR 1 = 1}. Т.е. тот
- * пользователь, который знает SQL может ввести логическое выражение и передать условие, которое 100% вернёт true,
- * следовательно, таким образом вместо того чтобы получить наши билеты по id 2, можем получить все наши билеты, потому
- * что условие true верно.
+ * <h1>PreparedStatement</h1>
+ * Это интерфейс, который наследуется от {@code Statement}, следовательно, он может всё тоже самое, что и
+ * {@code Statement}. В этом интерфейсе много сеттеров в отличии от родителя.
  * <br><br>
- * <b>Следовательно,</b> можно сделать вывод, что SQL Injection это техника, которая может нанести вред или уничтожить
- * базу данных, потому что можем кроме как логических условий делать и более сложные условия, например такие как DROP.
+ * Перепишем код, но при помощи {@code PreparedStatement}. Для этого создаём не
+ * <pre>{@code var statement = connection.createStatement();}</pre>
+ * ,а
+ * <pre>{@code var prepareStatement = connection.prepareStatement(sql);}</pre>
+ * У него есть много перегруженных методов, но суть их такова, что все они принимают первым параметром sql и затем,
+ * мы уже не должны передавать sql во все {@code execute()} методы.
  * <br><br>
- * <b>Для того чтобы обезопасить наш запрос, существует <i>PreparedStatement</i></b>, который проверяет все SQL
- * инъекции и не позволяет выполнять эти запросы.
+ * Далее, суть {@code preparedStatement(sql)} состоит в том, что все параметры мы не форматируем, а все неизвестные
+ * параметры мы перечисляем через знак вопроса:
+ * <pre>{@code
+ *         String sql = """
+ *                 SELECT id FROM flight_storage.ticket WHERE flight_id = ?
+ *                 """;
+ * }</pre>
+ * Те методы {@code set...();}, которые у интерфейса {@code PrepatedStatement} и нужны для того чтобы устанавливать
+ * эти значения в {@code ?}. Их нужно установить до того, как вызвали {@code prepareStatement.executeQuery()}:
+ * <pre>{@code prepareStatement.setLong(1, flightId);}</pre>
+ * Первый аргументом является номер вопросительного знака, а вторым параметром - его значение. Т.к. работаем с
+ * примитивными типами данных, нужно быть осторожным и если наш параметр, который пришёл в наш метод может быть
+ * {@code null}, тогда лучше воспользоваться {@code setObject()}, хотя в таком случае у нас тогда запрос будет невалидным,
+ * потому что на {@code null} все sql операции используют isNull или isNotNull не равно.
  */
 public class JdbcRunner {
-
-    /**
-     * Используя
-     * <pre>{@code String flightId = "2 OR 1 = 1; DROP TABLE info;";}</pre>
-     * получим ошибку, потому что последняя операция DROP TABLE и ResultSet ввиду этого не возвращается.
-     * @param args
-     */
     public static void main(String[] args) {
-        // String flightId = "2";
-        // String flightId = "2 OR 1 = 1";
-        String flightId = "2 OR 1 = 1; DROP TABLE info;";
-        var result = getTicketsByFlightId(flightId);
-        System.out.println(result);
+        // С начала дня 2020-1-01 по сегоднешнее число
+        var flightsBetweenResult = getFlightsBetween(
+                LocalDate.of(2020, 1, 1).atStartOfDay(),
+                LocalDateTime.now()
+        );
+        System.out.println(flightsBetweenResult);
     }
 
     /**
-     * <h2>Получаем список билетов по заданному <i>flightId</i></h2>
-     * getLong может вернуть null, что неправльно, т.к. примитив всегда должен быть не пустым. Здесь лучше подойдёт
-     * getObject, который принимает название колонки и тип, в который хотим преобразовать колонку. В таком случае он
-     * прекрасно работает с null'ами. Потому что null характерны для сложных типов, а не для примитивных. Поэтому
-     * использум {@code getObject()}
-     * <pre>{@code result.add(resultSet.getObject("id", Long.class));}</pre>
-     * @param flightId
-     * @return
-     * Получаем строки из таблицы по заданному ключу
+     * Для {@code PreparedStatement} могут выполняться любые запросы. <b>Более того,</b> {@code PreparedStatement}
+     * очень хорошо работает с разными типами данных, он сам знает, где нужно поставить одинарные кавычки для строк,
+     * либо есди работаем с датами, то знает как преобразовать дату в нужную строку, которая понимает нашу базу
+     * данных.
+     * <h2>Найдём перелёты между двумя датами</h2>
+     *
+     * @param start начальная дата
+     * @param end   конечная дата
+     * @return возвращает все наши id перелётов
      */
-    private static List<Long> getTicketsByFlightId(String flightId) {
+    private static List<Long> getFlightsBetween(LocalDateTime start, LocalDateTime end) {
         String sql = """
-                SELECT id FROM flight_storage.ticket WHERE flight_id = %s
-                """.formatted(flightId);
+                SELECT id FROM flight_storage.flight WHERE departure_date BETWEEN ? AND ?
+                """;
+
+        List<Long> result = new ArrayList<>();
+
+        try (
+                var connection = ConnectionManager.open();
+                var prepareStatement = connection.prepareStatement(sql);
+        ) {
+            // Устанавливаем значения
+            System.out.println(prepareStatement);
+            prepareStatement.setTimestamp(1, Timestamp.valueOf(start));
+            System.out.println(prepareStatement);
+            prepareStatement.setTimestamp(2, Timestamp.valueOf(end));
+            System.out.println(prepareStatement);
+
+            var resultSet = prepareStatement.executeQuery();
+
+            // Сохраняем ID перелётов, пробегаем по resultSet
+            while (resultSet.next()) {
+                result.add(resultSet.getLong("id"));
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return result;
+    }
+
+    private static List<Long> getTicketsByFlightId(Long flightId) {
+        String sql = """
+                SELECT id FROM flight_storage.ticket WHERE flight_id = ?
+                """;
         List<Long> result = new ArrayList<>();
         try (
                 var connection = ConnectionManager.open();
-                var statement = connection.createStatement();
+                var prepareStatement = connection.prepareStatement(sql);
         ) {
-            var resultSet = statement.executeQuery(sql);
+            prepareStatement.setLong(1, flightId);
+            var resultSet = prepareStatement.executeQuery();
 
             while (resultSet.next()) {
                 result.add(resultSet.getObject("id", Long.class));
