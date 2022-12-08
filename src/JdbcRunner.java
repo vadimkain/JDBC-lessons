1e03958f2ecd5c8c98e415db132172cbe4f2be93
vadimkain <48841785+vadimkain@users.noreply.github.com>
2022-12-08 12:14:33 +0200

    7. ResultSet. Операция SELECT

--- src/JdbcRunner.java
+++ src/JdbcRunner.java
@@ -4,35 +4,66 @@ import util.ConnectionManager;
 import java.sql.SQLException;
 
 /**
- * <h1>Statement. DML операции</h1>
- * Дело в том, что через execute мы получаем только true или false. Но помним, что стандарты SQL говорят о том, что
- * при выполнении INSERT/UPDATE/DELETE нам возвращается количество обновлённых/вставленных/удалённых строк. Через
- * операцию execute нам пришлось бы доплнительно вызывать метод (<i>execute.getUpdateCount()</i>).
+ * <h1>ResultSet. Операция SELECT</h1>
+ * Это самая сложная операция по манипулированию с Java-объектами для того чтобы получить результат нашей выборки.
  * <br><br>
- * На практике для DML операций чаще используется <b>executeUpdate()</b>.
+ * <b>executeQuery()</b> возвращает ResultSet.
+ * <br><br>
+ * Мы получили некую реализацию из нашего драйвера: <i>org.postgresql.jdbc.PgResultSet@12cdcf4</i>
+ * <br><br>
+ * Интерфейс <b>ResultSet</b> наследуется от AutoCloseable, а значит его тоже надо закрывать. Более того, он
+ * содержит невероятно большое количество методов для того чтобы манипулировать нашими данными. Кроме того чтобы
+ * получать какие-то данные из нашего результата выборки (много get методов), есть ещё и методы которые не стоит
+ * использовать (<i>update/insert</i>).
+ * <br><br>
+ * ResultSet очень похож на итератор (<b>Java Core</b>: Iterator это интерфейс, в котором есть два основных метода -
+ * hasNext() и next(). Реализация этого интерфейса проходится по структуре данных и проверяется через hasNext, есть
+ * ли следующий элемент в коллекции. Если есть, то через метод next получаем этот объект).
  * <br>
+ * В случае с ResultSet мы так же проходимся по результатам выборки (а это перечень строк), но в случае с ResultSet
+ * у нас есть только один метод: <b>next():boolean</b>. По сути, next() совмещает в себе и hasNext и next нашего
+ * итератора, только в данном случае нам достаточно будет его одного. Следовательно, если вернемся в реализацию
+ * и попробуем вывести результат нашей выборки, то используем цикл, если ожидаем, что получим много записей.
+ * А если берём только какое-то поле по ключу, то ожидаем какую-то одну строку.
+ * <br><br>
+ * <h2>Как устроен ResultSet</h2>
+ * <img src='ResultSet.png'/>
+ * <br>
+ * Как только сделали нашу выборку (вызвали метод executeQuery), то получили объект ResultSet и он стоит как будто
+ * перед результатом нащей выборки. И так как он очень похож на итератор, то для того чтобы узнать, есть ли
+ * какой-то результат у нас, то мы должны вызывать next, который вернёт true или false до того как мы не вызвали
+ * next первый раз, мы не имеем права вызывать get'теры. Как только вызвали next(), он переходит на следующую
+ * итерацию и становится будто <b>между строками</b>, и теперь чере геттеры можем получать значения из строки.
+ * Далее, после того как всё достали опять делаем запрос next() и опять переводим ResultSet на состояние между строк,
+ * и вновь можем выполнять геттеры из только что пройденной строки и т.д., пока следующий вызов next не вернёт нам
+ * false.
+ * <br><br>
+ * Более того, можно делать DML операции, но лучше так не делать, т.к. executeQuery в основном используется для
+ * SELECT'ов.
  */
 public class JdbcRunner {
     public static void main(String[] args) {
         Class<Driver> driverClass = Driver.class;
-        /*
         String sql = """
-                INSERT INTO info (data) VALUES ('Test1'), ('Test2'), ('Test3'), ('Test4');
+                SELECT * FROM flight_storage.ticket
                 """;
-         */
-        String sql = """
-                UPDATE info SET data = 'TestTest' WHERE id = 5 RETURNING *;
-                """; // RETURNING * возвращаем сразу же значения обновленных строк, но с executeUpdate не работает
         try (
                 var connection = ConnectionManager.open();
                 var statement = connection.createStatement();
         ) {
             System.out.println(connection.getSchema());
             System.out.println(connection.getTransactionIsolation());
-            var executeResult = statement.executeUpdate(sql); // Выполнение DML запроса
-            System.out.println(executeResult);
-            // Возвращаем кол-во строк, над которыми выполнили операции
-            System.out.println(statement.getUpdateCount());
+
+            // SELECT запрос
+            var executeResult = statement.executeQuery(sql);
+
+            while (executeResult.next()) {
+                System.out.println(
+                        executeResult.getLong("id") + " " + // Возвращаем id
+                        executeResult.getString("passenger_no") + " " + // Возвращаем passenger_no
+                        executeResult.getBigDecimal("cost") // Возвращаем cost
+                );
+            }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
