6397febaa273d2c6fed03e30d4d1a537b1cc28f5
vadimkain <48841785+vadimkain@users.noreply.github.com>
2022-12-10 17:59:37 +0200

    4. Batch запросы

--- src/TransactionRunner.java
+++ src/TransactionRunner.java
@@ -1,103 +1,87 @@
 import util.ConnectionManager;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
- * <h1>Транзакции и блокировки</h1>
- * Транзакция - это единица работы в рамках соединения с базой данных. Другими словами говоря, если мы хотим выполнить
- * совершенно любой sql запрос в нашу СУБД, то нам необходимо открыть транзакцию, выполнить этот запрос и закрыть её.
- * Но если у нас появляется какая-то ошибка во время выполнения, то мы должны сделать rollback, следовательно, у
- * транзакции есть только два состояния - либо <b>commit</b>, когда она (транзакция) полностью выполняется, либо
- * <b>rollback</b>, когда полностью всё откатывается. Поэтому транзакция атомарны - либо выполняются все запросы в
- * рамках текущей транзакции, либо не выполняются ни одного.
+ * <h1>Batch запросы</h1>
+ * Представим такую ситуацию: у нас есть четыре разных запроса:
+ * <ol>
+ *     <li>Вставляем какую-ту запись в таблицу airport</li>
+ *     <li>Вставляем запись в таблицу flight</li>
+ *     <li>Вставляем запись в таблицу ticket</li>
+ *     <li>Обновляем departure_date поле в таблице flight</li>
+ * </ol>
+ * <img src="../Batch1.png" />
+ * <img src="../Batch2.png" />
+ * <img src="../Batch3.png" />
+ * <br>
+ * Т.е. по сути, выполнилось четыре разных запросов, четыре разных транзакций. Более того, мы четыре раза сходили в БД
+ * и на это ушло определённое время. Как сходить один раз на сервер базы данных, где бы он не находился и сразу же
+ * выполнить все четыре запроса? По сути, мы бы открыли одну транзакцию, отправили все четыре запроса, которые хотим
+ * выполнить и вернулись с ними. Для этого как раз таки используются <b>Batch-запросы</b>.
  * <br><br>
- * <img src="../Транзакции1.png" />
- * <h1>Это класс, в котором выполняем транзакции</h1>
+ * Так мы ходим в базу данных два раза:
+ * <pre>{@code
+ *             int deletedTicketsResult = deleteTicketsStatement.executeUpdate();
+ *             if (true) {
+ *                 throw new RuntimeException("Ooops");
+ *             }
+ *             int deleteFlightResult = deleteFlightStatement.executeUpdate();
+ * }</pre>
+ * Для того чтобы не ходить два раза в базу данных, нам надо использовать не
+ * {@code connection.prepareStatement();}, а {@code connection.statement();}. Только он позволяет использовать
+ * <i>Batch</i> запросы.
+ * <br><br>
+ * После того, как открыли соединение и создали statement ({@code statement = connection.createStatement();} ) вызываем
+ * метод {@code .addBatch(sql)} и добавляем какой-нибудь SQL запрос:
+ * <pre>{@code
+ *             statement = connection.createStatement();
+ *             statement.addBatch(deleteTicketsSql);
+ *             statement.addBatch(deleteFlightSql);
+ * }</pre>
+ * Мы не можем выполнять {@code .addBatch(sql)} с {@code PreparedStatement} потому что он при создании сразу вызывает
+ * SQL запрос.
+ * <br><br>
+ * В тоже время, теперь нам не подходит работа с SQL запросами через вопросительный знак, потому что должны сами
+ * добавлять в конце наши <i>flight_id</i>:
+ * <pre>{@code
+ *         var deleteFlightSql = "DELETE FROM flight_storage.flight WHERE id = " + flightId;
+ *         var deleteTicketsSql = "DELETE FROM flight_storage.ticket WHERE id = " + flightId;
+ * }</pre>
+ * И выполняем их батчем и он выполняет все наши батч-запросы:
+ * <pre>{@code int[] ints = statement.executeBatch();}</pre>
+ * Результатом выполнения возвращается массив обновлённых значений. Т.е. для каждого выполненного запроса возвращается
+ * результат его выполнения. <b>Батчи используют в основном для операций DDL.</b>
+ * <br><br>
+ * Т.к. всё выполняется в рамках одной транзакции, - либо выполнятся все запросы, либо ни одной.
+ * <br><br>
+ * Тут поведение такое же, как и у обычных транзакций, только в данном случае экономим время на пересылку каждого
+ * из этих запросов, потому что посылаем их батчем, т.е. сразу всем скоупом.
+ * <br><br>
+ * <b>Batch очень хорошая вещь, если необходимо сэкономить время на отправки запрсов на сторону сервера базы данных.</b>
  */
 public class TransactionRunner {
-    /**
-     * <b>Напишем запрос, который будет удалять перелёты по его id</b>
-     * <br><br>
-     * У нас вылезет ошибка, потому что в таблице flight есть поле, которое ссылается на ticket. Как правило,
-     * для таких ситуаций указывается CASCADE ON DELETE (при удалении родительской записи - все строки, которые
-     * ссылаются на таблицу будут удалены автоматически), но в нашем случае это не подходит, потому что не добавили
-     * такое удаление. Следовательно, нам нужно сделать второй запрос, который удалит все записи из таблицы ticket,
-     * перед тем как удалять все записи из таблицы flight. Поэтому напишем второй запрос:
-     * <pre>{@code var deleteTicketsSql = "DELETE FROM flight_storage.ticket WHERE id = ?";}</pre>
-     * Представим, что у нас есть какая-то исключительная ситуация, т.е. во время выполнения нашего запроса на удаление
-     * ticket-ов все прошло успешно, но во время выполнения удаления flight произошел какой-нибудь Exception,
-     * следовательно, все наши ticket-ы будут удалены, а flight, который в принципе и хотели удалить - останется,
-     * потому что все запросы по умолчанию выполняются в рамках транзакций. Если сделаем что=то вроде такого:
-     * <pre>{@code
-     *             deleteTicketsStatement.executeUpdate();
-     *             if (true) {
-     *                 throw new RuntimeException("Ooops");
-     *             }
-     *             deleteFlightStatement.executeUpdate();
-     * }</pre>
-     * , в таком случае у нас 100% удалятся ticket-ы, но не удалятся flight, чего мы <b>не хотим</b> делать.
-     * Следовательно, нам надо как-то исправить эту ситуацию и удалить всё сразу, либо не удалять ничего. Это и есть
-     * атомарность, которая предоставляется транзакциями.
-     * <br><br>
-     * Чтобы это сделать, для начала нам надо убрать AUTO_COMMIT_MODE, т.е. чтобы каждый из наших запросов не выполнялся
-     * автоматически, мы должны у нашего соединения вызвать метод {@code connection.setAutoCommit(false);} и установить
-     * {@code false}, потому что по дефолту он {@code true}. <b>Делать это надо в самом начале, до выполнения любых
-     * запросов!</b> Таким образом, мы берём управление транзакциями на себя.
-     * <br><br>
-     * Теперь, выполняя все наши запросы {@code executeUpdate()} для нашего удаления ticket-ов и flight-ов, мы после
-     * всех запросов должны делать коммит: {@code connection.commit();}, т.е. фиксирование транзакций. <b>Нельзя
-     * вызвать методы {@code commit()} и {@code rollback()} если стоит {@code AutoCommit(true)} - будет исключение.</b>
-     * <br><br>
-     * Таким образом, теперь только после вызова {@code connection.commit()} у нас произойдет фиксирование нашей
-     * транзакции и удалятся все записи из соответствующих таблиц, но в случае исключения мы должны отловить его и
-     * вызвать у того же соединения метод {@code rollback()}.
-     * <br><br>
-     * <b>Таким образом, то что мы написали</b> - мы во время удаления наших ticket-ов
-     * {@code deleteTicketsStatement.executeUpdate();} и потом последующим пробрасыванием какого-то Exception-а:
-     * <pre>{@code
-     *             if (true) {
-     *                 throw new RuntimeException("Ooops");
-     *             }
-     * }</pre>
-     * откатим всю нашу транзакцию:
-     * <pre>{@code
-     *         catch (Exception e) {
-     *             // connection может быть неинициализирован, поэтому проверяем на null
-     *             if (connection != null) {
-     *                 connection.rollback();
-     *             }
-     *             throw e;
-     *         }
-     * }</pre>
-     * и не произойдёт фиксирование {@code connection.commit();}.
-     */
     public static void main(String[] args) throws SQLException {
         long flightId = 8;
 
-        var deleteFlightSql = "DELETE FROM flight_storage.flight WHERE id = ?";
-        var deleteTicketsSql = "DELETE FROM flight_storage.ticket WHERE id = ?";
+        var deleteFlightSql = "DELETE FROM flight_storage.flight WHERE id = " + flightId;
+        var deleteTicketsSql = "DELETE FROM flight_storage.ticket WHERE id = " + flightId;
 
         Connection connection = null;
-        PreparedStatement deleteFlightStatement = null;
-        PreparedStatement deleteTicketsStatement = null;
+        Statement statement = null;
 
         try {
             connection = ConnectionManager.open();
-            deleteFlightStatement = connection.prepareStatement(deleteFlightSql);
-            deleteTicketsStatement = connection.prepareStatement(deleteTicketsSql);
-
             connection.setAutoCommit(false);
 
-            deleteFlightStatement.setLong(1, flightId);
-            deleteTicketsStatement.setLong(1, flightId);
+            statement = connection.createStatement();
 
-            deleteTicketsStatement.executeUpdate();
-            if (true) {
-                throw new RuntimeException("Ooops");
-            }
-            deleteFlightStatement.executeUpdate();
+            statement.addBatch(deleteTicketsSql);
+            statement.addBatch(deleteFlightSql);
+
+            int[] ints = statement.executeBatch();
 
             connection.commit();
         } catch (Exception e) {
@@ -110,12 +94,6 @@ public class TransactionRunner {
             // Закрываем соединение
             if (connection != null) {
                 connection.close();
-            }
-            if (deleteFlightStatement != null) {
-                deleteFlightStatement.close();
-            }
-            if (deleteTicketsStatement != null) {
-                deleteFlightStatement.close();
             }
         }
     }
