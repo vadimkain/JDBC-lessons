2896d2e5c0e332f622010acab179f89dafb172f0
vadimkain <48841785+vadimkain@users.noreply.github.com>
2022-12-14 15:31:02 +0200

    4. DAO. Операции UPDATE и SELECT

--- src/DaoRunner.java
+++ src/DaoRunner.java
@@ -2,28 +2,58 @@ import dao.TicketDao;
 import entity.Ticket;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
 
 public class DaoRunner {
     public static void main(String[] args) {
-
+        findAllTest();
     }
 
-    private static void deleteTest() {
+    private static void findAllTest() {
+        List<Ticket> tickets = TicketDao.getInstance().findAll();
+        for (Ticket ticket :
+                tickets) {
+            System.out.println(ticket);
+        }
+    }
+
+    private static void findAndUpdateTest(Long id) {
         TicketDao ticketDao = TicketDao.getInstance();
 
-        boolean deleteResult = ticketDao.delete(59L);
+        Optional<Ticket> maybeTicket = ticketDao.findById(id);
+
+        System.out.println(maybeTicket);
+
+        // Если maybeTicket существует (не Optional.empty), то обновляем cost:
+        maybeTicket.ifPresent(ticket -> {
+            ticket.setCost(BigDecimal.valueOf(188.88));
+            ticketDao.update(ticket);
+        });
+    }
+
+    private static void deleteTest(Long id) {
+        TicketDao ticketDao = TicketDao.getInstance();
+
+        boolean deleteResult = ticketDao.delete(id);
         System.out.println(deleteResult);
     }
 
-    private static void saveTest() {
+    private static void saveTest(
+            String passengerNo,
+            String passengerName,
+            Long flightId,
+            String seatNo,
+            BigDecimal cost
+    ) {
         TicketDao ticketDao = TicketDao.getInstance();
 
         Ticket ticket = new Ticket();
-        ticket.setPassengerNo("1234567");
-        ticket.setPassengerName("Test");
-        ticket.setFlightId(3L);
-        ticket.setSeatNo("B3");
-        ticket.setCost(BigDecimal.TEN);
+        ticket.setPassengerNo(passengerNo);
+        ticket.setPassengerName(passengerName);
+        ticket.setFlightId(flightId);
+        ticket.setSeatNo(seatNo);
+        ticket.setCost(cost);
 
         Ticket saveTicket = ticketDao.save(ticket);
         System.out.println(saveTicket);
--- src/dao/TicketDao.java
+++ src/dao/TicketDao.java
@@ -5,6 +5,9 @@ import exception.DaoException;
 import util.ConnectionManager;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * <h1>DAO. Операции DELETE и INSERT</h1>
@@ -19,8 +22,6 @@ import java.sql.*;
  * TicketDao.
  */
 public class TicketDao {
-    private static final TicketDao INSTANCE = new TicketDao();
-
     private static final String DELETE_SQL = """
             DELETE FROM flight_storage.ticket WHERE id = ?
             """;
@@ -28,12 +29,91 @@ public class TicketDao {
             INSERT INTO flight_storage.ticket (passenger_no, passenger_name, flight_id, seat_no, cost)
             VALUES (?, ?, ?, ?, ?);
             """;
+    public static final String UPDATE_SQL = """
+            UPDATE flight_storage.ticket 
+            SET passenger_no = ?, passenger_name = ?, flight_id = ?, seat_no = ?, cost = ?
+            WHERE id = ?
+            """;
+    private static final String FIND_ALL_SQL = """
+            SELECT id, passenger_no, passenger_name, flight_id, seat_no, cost
+            FROM flight_storage.ticket
+            """;
+    public static final String FIND_BY_ID_SQL = FIND_ALL_SQL + """
+             WHERE id = ?
+            """;
 
-    private TicketDao() {
+    public List<Ticket> findAll() {
+        try (
+                Connection connection = ConnectionManager.get();
+                PreparedStatement preparedStatement = connection.prepareStatement(FIND_ALL_SQL);
+        ) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            List<Ticket> tickets = new ArrayList<>();
+
+            while (resultSet.next()) {
+                tickets.add(buildTicket(resultSet));
+            }
+
+            return tickets;
+        } catch (SQLException throwables) {
+            throw new DaoException(throwables);
+        }
     }
 
-    public static TicketDao getInstance() {
-        return INSTANCE;
+    /**
+     * <b>Если метод может вернуть {@code null}, то мы должны возвращать {@code Optional<>}. В случае
+     * с коллекциями, мы возвращаем пустую коллекцию. <i>Это правило хорошего тона</i>.</b>
+     */
+    public Optional<Ticket> findById(Long id) {
+        try (
+                Connection connection = ConnectionManager.get();
+                PreparedStatement preparedStatement = connection.prepareStatement(FIND_BY_ID_SQL);
+        ) {
+            preparedStatement.setLong(1, id);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            Ticket ticket = null;
+
+            if (resultSet.next()) {
+                ticket = buildTicket(resultSet);
+            }
+
+            return Optional.ofNullable(ticket);
+        } catch (SQLException throwables) {
+            throw new DaoException(throwables);
+        }
+    }
+
+    private static Ticket buildTicket(ResultSet resultSet) throws SQLException {
+        return new Ticket(
+                resultSet.getLong("id"),
+                resultSet.getString("passenger_no"),
+                resultSet.getString("passenger_name"),
+                resultSet.getLong("flight_id"),
+                resultSet.getString("seat_no"),
+                resultSet.getBigDecimal("cost")
+        );
+    }
+
+    public void update(Ticket ticket) {
+        try (
+                Connection connection = ConnectionManager.get();
+                PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_SQL);
+        ) {
+            preparedStatement.setString(1, ticket.getPassengerNo());
+            preparedStatement.setString(2, ticket.getPassengerName());
+            preparedStatement.setLong(3, ticket.getFlightId());
+            preparedStatement.setString(4, ticket.getSeatNo());
+            preparedStatement.setBigDecimal(5, ticket.getCost());
+            preparedStatement.setLong(6, ticket.getId());
+
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new DaoException(e);
+        }
     }
 
     public Ticket save(Ticket ticket) {
@@ -78,4 +158,13 @@ public class TicketDao {
         }
     }
 
+
+    private static final TicketDao INSTANCE = new TicketDao();
+
+    private TicketDao() {
+    }
+
+    public static TicketDao getInstance() {
+        return INSTANCE;
+    }
 }
